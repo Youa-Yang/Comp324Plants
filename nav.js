9cb435474b3b531e12db250bb177993e1230e943
Youa-Yang <94993902+Youa-Yang@users.noreply.github.com>
2022-05-01 13:35:39 -0500

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,28 +1,59 @@
 const createTopNav = () => {
-let topnav= document.querySelector('.topnav')
+let topnav = document.querySelector('.topnav')
+
 
 topnav.innerHTML =`
-    <div class = "nav">
-    <a href="#"><img src="img/logo.png" class="logo" alt=""></a>
-    <div class="nav-items">
-      <div class="search">
-      <input type="text" class="search-box" placeholder="search product">
-      <button class="search-btn">search</button>
-      <a href="index.html">Home</a>
-      <a href="Login.html"><i class="fa-solid fa-user"></i></a>
-      <a href="cart.html"><i class="fa-solid fa-cart-shopping"></i></a>
-      <a href="wishlist.html"><i class="fa-solid fa-list"></i></a>
-      </div>
-    </div>
-    <div class="center">
-      <ul id = "menutabs">
-         <li><a href="navigation/house-plants.html" target="_blank">house plants</a></li>
-         <li><a href="navigation/succulents.html" target="_blank">succulents</a></li>
-         <li><a href="navigation/planters.html" target="_blank">planters</a></li>
-         <li><a href="navigation/plant-accessories.html" target="_blank">plant accessories</a></li>
-       </ul>
-    </div>
-    </div>
+    <nav>
+        <ul>
+          <li><a href="home.html"><i class="fa-solid fa-house"></i></a></li>
+          <li><a href="search.html"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+          <li><a href="footer/Login.html"><i class="fa-solid fa-user"></i></a><li>
+          <li><a href="wishlist.html"><i class="fa-solid fa-list"></i></a><li>
+          <li><a href="navigation/house-plants.html">house plants</a></li>
+          <li><a href="navigation/succulents.html">succulents</a></li>
+          <li><a href="navigation/planters.html">planters</a></li>
+          <li><a href="navigation/plant-accessories.html">plant accessories</a></li>
+        </ul>
+      </nav>
+      <style>
+        nav{
+          display: grid;
+          font size: 200px;
+        }
+        .nav a{
+          float: left;
+          text-decoration: none;
+          width: 8%;
+          text-align: center;
+        }
+
+        ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: gray;
+        }
+
+        li {
+        display: block;
+        float: left;
+        margin: 0;
+        border: 10px white;
+        }
+
+        li a {
+        display: flex;
+        color: black;
+        text-align: left;
+        padding: 10px;
+        text-decoration: none;
+        }
+
+        li a:hover {
+        background-color: white;
+        }
+      </style>
   `;
 }
 
